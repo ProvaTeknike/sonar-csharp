a4054e3387acbc948b27bed3e999f700f1f63f08
Amaury Levé <amaury.leve@sonarsource.com>
2017-10-20 15:42:44 +0200

    Fix S3242: Rule should not suggest base type resulting in inconsistent accessibility

--- sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/MethodsShouldUseBaseTypes.cs
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/MethodsShouldUseBaseTypes.cs
@@ -691,3 +691,33 @@ namespace Test_24
         }
     }
 }
+
+namespace Something
+{
+    internal interface IFoo
+    {
+        bool IsFoo { get; }
+    }
+
+    public class Foo : IFoo
+    {
+        public bool IsFoo { get; set; }
+    }
+}
+
+// Test that rule doesn't suggest base with inconsistent accessibility
+namespace Test_25
+{
+    public class Bar
+    {
+        public void MethodOne(Something.Foo f)
+        {
+            var x = f.IsFoo;
+        }
+
+        private void MethodOne(Something.Foo f) // Noncompliant
+        {
+            var x = f.IsFoo;
+        }
+    }
+}
